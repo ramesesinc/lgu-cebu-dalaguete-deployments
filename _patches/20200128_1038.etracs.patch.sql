591edb715c9eec869637e3b7d7d346bdaa6102b8
worgflores <worgflores@gmail.com>
2020-01-28 10:38:58 +0800

    added patch

---
+++ _patches/20200128_1038.etracs.patch.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `cashreceipt_plugin` (
+  `objid` varchar(50) NOT NULL,
+  `connection` varchar(100) NOT NULL,
+  `servicename` varchar(255) NOT NULL,
+  constraint pk_cashreceipt_plugin PRIMARY KEY (`objid`)
+  KEY `ix_connection` (`connection`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+; 
+
