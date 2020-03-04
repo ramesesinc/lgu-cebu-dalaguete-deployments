b137526d158c5e0a09ceefa631ea1870bce059a7
worgflores <worgflores@gmail.com>
2020-03-04 17:12:59 +0800

    updated patch

--- _res/patches/20200304/patch_etracs_01.sql
+++ _res/patches/20200304/patch_etracs_01.sql
@@ -32,3 +32,39 @@ alter table cashreceipt_groupitem add CONSTRAINT fk_cashreceipt_groupitem_parent
 	FOREIGN KEY (parentid) REFERENCES cashreceipt_group (objid) 
 ;
 
+
+drop view if exists vw_income_summary
+; 
+create view vw_income_summary as 
+select 
+  inc.*, fund.groupid as fundgroupid, 
+  ia.objid as itemid, ia.code as itemcode, ia.title as itemtitle, ia.type as itemtype  
+from income_summary inc 
+  inner join fund on fund.objid = inc.fundid 
+  inner join itemaccount ia on ia.objid = inc.acctid 
+;
+
+
+drop view if exists sys_user_role
+; 
+create view sys_user_role AS 
+select  
+	u.objid AS objid, 
+	u.lastname AS lastname, 
+	u.firstname AS firstname, 
+	u.middlename AS middlename, 
+	u.username AS username,
+	concat(u.lastname, ', ', u.firstname, (case when u.middlename is null then '' else concat(' ', u.middlename) end)) AS name, 
+	ug.role AS role, 
+	ug.domain AS domain, 
+	ugm.org_objid AS orgid, 
+	u.txncode AS txncode, 
+	u.jobtitle AS jobtitle, 
+	ugm.objid AS usergroupmemberid, 
+	ugm.usergroup_objid AS usergroup_objid,
+	ugm.org_objid as respcenter_objid, 
+	ugm.org_name as respcenter_name 
+from sys_usergroup_member ugm 
+	inner join sys_usergroup ug on ug.objid = ugm.usergroup_objid 
+	inner join sys_user u on u.objid = ugm.user_objid 
+;
